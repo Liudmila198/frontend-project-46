2ee47b0fb155049c8e3523173c78dc92616918dc
liudmila198 <belokopytova.ln@gmail.com>
2025-06-18 11:57:40 +0300

    4

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,11 +8,5 @@ program
   .name('gendiff')
   .description('Compares two configuration files and shows a difference.')
   .version('1.0.0', '-V, --version', 'output the version number')
-  .argument('<filepath1>', 'path to first file')
-  .argument('<filepath2>', 'path to second file')
-  .action((filepath1, filepath2) => {
-    const diff = genDiff(filepath1, filepath2)
-    console.log(diff);
-  })
 
 program.parse()
