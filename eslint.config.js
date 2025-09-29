f672c04a285064e7e5508e2bc3d78b7d3f18df4f
liudmila198 <belokopytova.ln@gmail.com>
2025-09-29 14:09:22 +0300

    70

--- eslint.config.js
+++ eslint.config.js
@@ -16,7 +16,7 @@ export default [
       },
     },
     rules: {
-      'indent': ['error', 2],
+      'indent': ['error', 4], // Change from 2 to 4
       'linebreak-style': ['error', 'unix'],
       'quotes': ['error', 'single'],
       'semi': ['error', 'never'],
