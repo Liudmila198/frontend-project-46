566687dbc16da4b42389c970587a36ddf8190040
liudmila198 <belokopytova.ln@gmail.com>
2025-09-29 18:40:12 +0300

    72

--- eslint.config.js
+++ eslint.config.js
@@ -1,8 +1,12 @@
 import js from '@eslint/js'
+import stylistic from '@stylistic/eslint-plugin'
 
 export default [
   js.configs.recommended,
   {
+    plugins: {
+      '@stylistic': stylistic
+    },
     languageOptions: {
       globals: {
         console: 'readonly',
@@ -16,11 +20,16 @@ export default [
       },
     },
     rules: {
-      'indent': ['error', 2], // Change from 2 to 4
-      'linebreak-style': ['error', 'unix'],
-      'quotes': ['error', 'single'],
-      'semi': ['error', 'never'],
+      'indent': 'off',
+      'linebreak-style': 'off',
+      'quotes': 'off',
+      'semi': 'off',
       'no-console': 'off',
+      '@stylistic/indent': ['error', 2],
+      '@stylistic/linebreak-style': ['error', 'unix'],
+      '@stylistic/quotes': ['error', 'single'],
+      '@stylistic/semi': ['error', 'never'],
+      '@stylistic/eol-last': ['error', 'always'],
     },
     ignores: [
       '**/node_modules/',
